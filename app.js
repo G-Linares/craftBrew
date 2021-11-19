e941d838ebbdc8ec4ae440a48fb0cd4a77ff8326
G-Linares <gerrylinares@gmail.com>
2021-11-18 22:25:30 -0600

    Heroku deployment

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => console.log('Server mounted on port 3000'));
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`Servidor montado en puerto 3000`);
+});
 
 app.use(express.static('public'));
 
