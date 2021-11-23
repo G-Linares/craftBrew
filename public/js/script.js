d634e68ff514288c143d073f94e995ddbc5bd246
G-Linares <gerrylinares@gmail.com>
2021-11-23 01:18:04 -0600

    Added Login, animation on logos, changes to typed and cart

--- public/js/script.js
+++ public/js/script.js
@@ -72,3 +72,9 @@ function moveToPrevSlide() {
   updateSlidePosition();
 }
 
+function openLoginForm(){
+  document.body.classList.add("showLoginForm");
+}
+function closeLoginForm(){
+  document.body.classList.remove("showLoginForm");
+}
--- public/js/typed.js
+++ public/js/typed.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.typed', {
-    strings: ["Cheap", "Delicious", "Expendable"],
+    strings: ["Cheap", "Delicious", "Tasteful"],
     suffle:false,
     smartBackspace: true,
     typeSpeed: 50,
